dd5f3713ce1bd62758c51c5ce8494861e67c1daf
Clément Vicart <clem.vicart@gmail.com>
2026-01-24 16:02:32 +0100

    feat: Add option to select currency (#7)
    * feat: Add option to select currency
    
    * feat: Add option to select currency

--- kmp/src/commonMain/kotlin/dev/vicart/pixelcount/ui/screens/AddExpenseGroup.kt
+++ kmp/src/commonMain/kotlin/dev/vicart/pixelcount/ui/screens/AddExpenseGroup.kt
@@ -14,8 +14,12 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.PersonAdd
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.ExposedDropdownMenuAnchorType
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.FilledTonalIconButton
 import androidx.compose.material3.Icon
@@ -33,6 +37,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.unit.dp
@@ -42,6 +47,7 @@ import dev.vicart.pixelcount.model.ExpenseGroup
 import dev.vicart.pixelcount.resources.Res
 import dev.vicart.pixelcount.resources.add_expense_group
 import dev.vicart.pixelcount.resources.john_doe
+import dev.vicart.pixelcount.resources.options
 import dev.vicart.pixelcount.resources.participant_name
 import dev.vicart.pixelcount.resources.participants
 import dev.vicart.pixelcount.resources.ski_vacations
@@ -53,7 +59,7 @@ import dev.vicart.pixelcount.ui.components.EmojiPicker
 import dev.vicart.pixelcount.ui.viewmodel.AddExpenseGroupViewModel
 import org.jetbrains.compose.resources.stringResource
 
-@OptIn(ExperimentalMaterial3ExpressiveApi::class)
+@OptIn(ExperimentalMaterial3ExpressiveApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun AddExpenseGroupScreen(
     onBack: () -> Unit,
@@ -179,6 +185,54 @@ fun AddExpenseGroupScreen(
                             }
                         )
                     )
+                }
+            }
+
+            Text(
+                text = stringResource(Res.string.options),
+                style = MaterialTheme.typography.titleMedium,
+                modifier = Modifier.padding(horizontal = 16.dp)
+            )
+
+            val currency by vm.currency.collectAsStateWithLifecycle()
+
+            var menuExpanded by remember(currency) { mutableStateOf(false) }
+
+            var query by remember(currency) { mutableStateOf(currency.displayName) }
+
+            ExposedDropdownMenuBox(
+                expanded = menuExpanded,
+                onExpandedChange = { menuExpanded = it },
+                modifier = Modifier.padding(horizontal = 16.dp)
+            ) {
+                OutlinedTextField(
+                    value = query,
+                    onValueChange = { query = it },
+                    leadingIcon = {
+                        Text(text = currency.symbol)
+                    },
+                    singleLine = true,
+                    modifier = Modifier.fillMaxWidth().menuAnchor(ExposedDropdownMenuAnchorType.PrimaryEditable),
+                    trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = menuExpanded) },
+                    colors = ExposedDropdownMenuDefaults.outlinedTextFieldColors()
+                )
+
+                ExposedDropdownMenu(
+                    expanded = menuExpanded,
+                    onDismissRequest = { menuExpanded = false }
+                ) {
+                    val availableCurrencies by vm.availableCurrencies.collectAsStateWithLifecycle(emptyList())
+
+                    availableCurrencies.filter { it.displayName.contains(query, true) }.forEach { currency ->
+                        DropdownMenuItem(
+                            text = { Text(currency.displayName) },
+                            onClick = { vm.currency.value = currency },
+                            contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding,
+                            leadingIcon = {
+                                Text(text = currency.symbol)
+                            }
+                        )
+                    }
                 }
             }
         }
