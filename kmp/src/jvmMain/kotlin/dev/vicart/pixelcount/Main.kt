c5a6a23a7e684f877bfe6bb721c8ac5218924c1e
Clément Vicart <clem.vicart@gmail.com>
2026-01-25 02:07:27 +0100

    Add image picker for JVM

--- kmp/src/jvmMain/kotlin/dev/vicart/pixelcount/Main.kt
+++ kmp/src/jvmMain/kotlin/dev/vicart/pixelcount/Main.kt
@@ -1,27 +1,33 @@
 package dev.vicart.pixelcount
 
-import androidx.compose.ui.graphics.painter.BitmapPainter
-import androidx.compose.ui.window.singleWindowApplication
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
 import dev.vicart.pixelcount.data.database.Database
 import dev.vicart.pixelcount.data.database.DriverFactory
 import dev.vicart.pixelcount.data.database.createDatabase
 import dev.vicart.pixelcount.resources.Res
 import dev.vicart.pixelcount.resources.favicon
 import dev.vicart.pixelcount.ui.App
-import org.jetbrains.compose.resources.decodeToImageBitmap
-import org.jetbrains.compose.resources.getDrawableResourceBytes
-import org.jetbrains.compose.resources.getSystemResourceEnvironment
+import org.jetbrains.compose.resources.painterResource
+import java.awt.Window
 
-suspend fun main() {
+lateinit var topWindow: Window
+
+fun main() {
     Database.init(createDatabase(DriverFactory()))
 
-    singleWindowApplication(
-        title = "PixelCount",
-        icon = getDrawableResourceBytes(getSystemResourceEnvironment(),
-            Res.drawable.favicon).let {
-                BitmapPainter(it.decodeToImageBitmap())
+    application {
+
+        Window(
+            onCloseRequest = ::exitApplication,
+            title = "",
+            icon = painterResource(Res.drawable.favicon)
+        ) {
+            LaunchedEffect(Unit) {
+                topWindow = window
+            }
+            App()
         }
-    ) {
-        App()
     }
 }
---
+++ kmp/src/jvmMain/kotlin/dev/vicart/pixelcount/platform/ContentPlatform.jvm.kt
@@ -0,0 +1,51 @@
+package dev.vicart.pixelcount.platform
+
+import dev.vicart.pixelcount.topWindow
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.io.File
+import javax.swing.JFileChooser
+import javax.swing.filechooser.FileNameExtensionFilter
+import kotlin.io.path.Path
+import kotlin.io.path.absolutePathString
+import kotlin.uuid.Uuid
+
+actual suspend fun pickImage(): ByteArray? {
+    val chooser = JFileChooser()
+    val filter = FileNameExtensionFilter("Image files", "png", "jpg", "jpeg")
+    chooser.fileFilter = filter
+    chooser.fileSelectionMode = JFileChooser.FILES_ONLY
+    chooser.isMultiSelectionEnabled = false
+
+    val returnVal = chooser.showOpenDialog(topWindow)
+    if(returnVal == JFileChooser.APPROVE_OPTION) {
+        return withContext(Dispatchers.IO) {
+            chooser.selectedFile.readBytes()
+        }
+    }
+    return null
+}
+
+actual suspend fun readImage(id: Uuid): ByteArray? = withContext(Dispatchers.IO) {
+    File(cacheDir.absolutePathString(), id.toString()).takeIf { it.exists() }?.readBytes()
+}
+
+actual suspend fun writeImage(id: Uuid, data: ByteArray) = withContext(Dispatchers.IO) {
+    File(cacheDir.absolutePathString(), id.toString()).writeBytes(data)
+}
+
+private val cacheDir = System.getProperty("os.name").lowercase().let {
+    when {
+        it.startsWith("linux") -> Path(System.getProperty("user.home"), ".cache", "pixelcount")
+        else -> throw RuntimeException("Host not supported")
+    }
+}
+
+actual suspend fun deleteImage(id: Uuid) = withContext(Dispatchers.IO) {
+    File(cacheDir.absolutePathString(), id.toString()).takeIf { it.exists() }?.delete()
+    Unit
+}
+
+actual suspend fun hasImage(id: Uuid): Boolean = withContext(Dispatchers.IO) {
+    File(cacheDir.absolutePathString(), id.toString()).exists()
+}
