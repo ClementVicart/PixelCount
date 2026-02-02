44d7369758419812f7ee336a75eac994b6783025
Clément Vicart <clem.vicart@gmail.com>
2026-02-02 18:31:53 +0100

    fix: NPE on WearOS with dynamic colors not available

---
+++ wearosApp/src/main/kotlin/dev/vicart/pixelcount/ui/theme/Color.kt
@@ -0,0 +1,60 @@
+package dev.vicart.pixelcount.ui.theme
+
+import androidx.compose.ui.graphics.Color
+import androidx.wear.compose.material3.ColorScheme
+
+val primaryDark = Color(0xFF91D5AD)
+val onPrimaryDark = Color(0xFF003822)
+val primaryContainerDark = Color(0xFF025233)
+val onPrimaryContainerDark = Color(0xFFACF2C8)
+val secondaryDark = Color(0xFFACD28E)
+val onSecondaryDark = Color(0xFF1A3705)
+val secondaryContainerDark = Color(0xFF304F1A)
+val onSecondaryContainerDark = Color(0xFFC8EEA8)
+val tertiaryDark = Color(0xFF88D6BA)
+val onTertiaryDark = Color(0xFF00382A)
+val tertiaryContainerDark = Color(0xFF00513E)
+val onTertiaryContainerDark = Color(0xFFA4F2D5)
+val errorDark = Color(0xFFFFB4AB)
+val onErrorDark = Color(0xFF690005)
+val errorContainerDark = Color(0xFF93000A)
+val onErrorContainerDark = Color(0xFFFFDAD6)
+val backgroundDark = Color(0xFF0F1511)
+val onBackgroundDark = Color(0xFFDFE4DD)
+val surfaceDark = Color(0xFF0F1511)
+val onSurfaceDark = Color(0xFFDFE4DD)
+val surfaceVariantDark = Color(0xFF404943)
+val onSurfaceVariantDark = Color(0xFFC0C9C0)
+val outlineDark = Color(0xFF8A938B)
+val outlineVariantDark = Color(0xFF404943)
+val surfaceContainerLowDark = Color(0xFF171D19)
+val surfaceContainerDark = Color(0xFF1B211D)
+val surfaceContainerHighDark = Color(0xFF262B27)
+
+val colorScheme = ColorScheme(
+    primary = primaryDark,
+    onPrimary = onPrimaryDark,
+    primaryContainer = primaryContainerDark,
+    onPrimaryContainer = onPrimaryContainerDark,
+    secondary = secondaryDark,
+    onSecondary = onSecondaryDark,
+    secondaryContainer = secondaryContainerDark,
+    onSecondaryContainer = onSecondaryContainerDark,
+    tertiary = tertiaryDark,
+    onTertiary = onTertiaryDark,
+    tertiaryContainer = tertiaryContainerDark,
+    onTertiaryContainer = onTertiaryContainerDark,
+    error = errorDark,
+    onError = onErrorDark,
+    errorContainer = errorContainerDark,
+    onErrorContainer = onErrorContainerDark,
+    background = backgroundDark,
+    onBackground = onBackgroundDark,
+    surfaceContainer = surfaceContainerDark,
+    onSurface = onSurfaceDark,
+    surfaceContainerLow = surfaceContainerLowDark,
+    surfaceContainerHigh = surfaceContainerHighDark,
+    onSurfaceVariant = onSurfaceVariantDark,
+    outline = outlineDark,
+    outlineVariant = outlineVariantDark
+)
--- wearosApp/src/main/kotlin/dev/vicart/pixelcount/ui/theme/Theme.kt
+++ wearosApp/src/main/kotlin/dev/vicart/pixelcount/ui/theme/Theme.kt
@@ -3,6 +3,7 @@ package dev.vicart.pixelcount.ui.theme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
+import androidx.wear.compose.material3.ColorScheme
 import androidx.wear.compose.material3.MaterialTheme
 import androidx.wear.compose.material3.MotionScheme
 import androidx.wear.compose.material3.dynamicColorScheme
@@ -13,11 +14,11 @@ fun AppTheme(
 ) {
     val context = LocalContext.current
     val scheme = remember(context) {
-        dynamicColorScheme(context)
+        dynamicColorScheme(context) ?: colorScheme
     }
 
     MaterialTheme(
-        colorScheme = scheme!!,
+        colorScheme = scheme,
         motionScheme = MotionScheme.expressive(),
         typography = typography,
         content = content
